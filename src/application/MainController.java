52b4f389d75bbea918ab97d16d237c5378b3ff9f
PATRICK <hotandtrendinginc2018@gmail.com>
2025-01-01 15:32:34 +0100

    Video Player App in First Stage, You can Pick Videos from your computer and Play

---
+++ src/application/MainController.java
@@ -0,0 +1,38 @@
+package application;
+import javafx.scene.media.MediaView;
+import javafx.fxml.FXML;
+import javafx.scene.media.*;
+import javafx.stage.FileChooser;
+import java.io.File;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+public class MainController {
+			@FXML
+			Button selectVideo;
+			@FXML
+			MediaView videoViewer;
+		
+	  	String source;
+	   
+	     
+	     public void openVideo(ActionEvent event) {
+	    	 FileChooser pickVideo=new FileChooser();
+	    	 pickVideo.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP4 or Mkv Files", "*.mp4",".mkv",".mov"));
+	    	 File selectedVideo= pickVideo.showOpenDialog(null);
+	    	 if (selectedVideo != null) {
+	             source = selectedVideo.toURI().toString();
+	             Media video = new Media(source);
+	             MediaPlayer videoPlayer = new MediaPlayer(video);
+	             videoPlayer.setAutoPlay(true);
+	             videoViewer.setMediaPlayer(videoPlayer);
+	         } else {
+	             System.out.println("No video selected.");
+	         }
+	     }
+	     @FXML
+	     public void playVideo() {
+	    	 System.out.println("Video Will Play");
+	     }
+	     
+	
+}
